69835d59c4965c7bb2ae04fd8c0062f49e7009d4
mlibasc <li.mbasc@gmail.com>
2020-01-27 20:21:13 -0500

    updated to save the gray scale picture

--- gray_scale.py
+++ gray_scale.py
@@ -4,8 +4,12 @@ import cv2
 #Load an image in grayscale
 cv2.namedWindow("Grayscale Picture", cv2.WINDOW_NORMAL)
 img = cv2.imread('1_30.jpg', 0)
-imgSized = cv2.resize(img, (2000,4000))
+imgSized = cv2.resize(img, (960,540))
 cv2.imshow("Grayscale Picture", imgSized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+k = cv2.waitKey(0) & 0xFF
+if k == ord('s'):
+    cv2.imwrite('Gray_Scaled_1_30.jpg', img)
+    cv2.destroyAllWindows()
+else:
+    cv2.destroyAllWindows()
 
